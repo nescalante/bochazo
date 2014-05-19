dd7a603b4dea5bcec3e3cfaea7d3241ca15ac740
Nicolás Escalante <nlante@gmail.com>
2014-05-19 16:27:21 -0300

    grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,12 +19,39 @@ module.exports = function (grunt) {
             test: {
                 src: ['test/*.js']
             }
+        },
+        copy: {
+            bootstrap: {
+                cwd: 'bower_components/bootstrap/less/',
+                src: '**',
+                dest: 'public/styles/bootstrap/',
+                expand: true
+            },
+            fonts: {
+                cwd: 'bower_components/bootstrap/dist/fonts/',
+                src: '**',
+                dest: 'public/styles/fonts/',
+                expand: true
+            },
+            variables: {
+                src: 'public/styles/site/variables.less',
+                dest: 'public/styles/bootstrap/variables.less'
+            }
+        },
+        less: {
+            development: {
+                files: {
+                    'public/styles/site.css': 'public/styles/site/site.less',
+                    'public/styles/bootstrap.css': 'public/styles/bootstrap/bootstrap.less',
+                }
+            }
         }
     });
 
     require('load-grunt-tasks')(grunt);
 
-    grunt.registerTask('lint', ['jshint', 'lesslint']);
+    grunt.registerTask('lint', ['copy:bootstrap', 'copy:fonts', 'copy:variables', 'jshint', 'lesslint']);
+    grunt.registerTask('compileless', [])
     grunt.registerTask('test', ['mochaTest']);
     grunt.registerTask('default', ['lint', 'test']);
     grunt.registerTask('build', ['lint', 'test']);
